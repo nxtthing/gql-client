66a325c4cb9e2e2dc7ee60fc4e554c343346002b
Alex Yakubenko <alexandr.yakubenko@gmail.com>
2023-05-31 13:29:34 +0300

    fix results-page init

--- lib/nxt_gql_client/results_page.rb
+++ lib/nxt_gql_client/results_page.rb
@@ -3,7 +3,7 @@ module NxtGqlClient
     attr_reader :total, :nodes
 
     def initialize(response)
-      @nodes = response["nodes"].map { |result| yield(result) }
+      @nodes = response["nodes"].map { |result| yield(result) } if response["nodes"]
       @total = response["total"]
     end
   end
