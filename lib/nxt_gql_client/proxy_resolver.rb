257ac182f26cce6396d3fdd2070b62a42bf90a04
Alex Yakubenko <alexandr.yakubenko@gmail.com>
2023-06-07 17:21:11 +0300

    add proxy_resolver

---
+++ lib/nxt_gql_client/proxy_resolver.rb
@@ -0,0 +1,16 @@
+module NxtGqlClient
+  module ProxyResolver
+    def resolve(**params)
+      query_name = self.class.name.demodulize.downcase
+      self.class.type.unwrap.proxy_model.send(query_name, resolver: self, **params)
+    rescue NxtSchedulingGql::InvalidResponse => exc
+      handle_invalid_response_error(exc)
+    end
+
+    protected
+
+    def handle_invalid_response_error(exc)
+      raise exc
+    end
+  end
+end
