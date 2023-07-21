b8a2383b3013d66e8b3fad9847cb19ba9feead57
Alex Yakubenko <alexandr.yakubenko@gmail.com>
2023-07-21 20:43:59 -0300

    refactoring

--- lib/nxt_gql_client/proxy_resolver.rb
+++ lib/nxt_gql_client/proxy_resolver.rb
@@ -2,15 +2,18 @@ require "nxt_gql_client/invalid_response"
 
 module NxtGqlClient
   module ProxyResolver
-    def resolve(proxy_model: nil, **params)
+    def resolve(**params)
+      resolve_proxy(proxy_model: Model.field_type(self.class).proxy_model, **params)
+    end
+
+    protected
+
+    def resolve_proxy(proxy_model:, **params)
       query_name = self.class.name.demodulize.underscore
-      proxy_model ||= Model.field_type(self.class).proxy_model
       proxy_model.send(query_name, resolver: self, **params)
     rescue InvalidResponse => exc
       handle_invalid_response_error(exc)
     end
-
-    protected
 
     def handle_invalid_response_error(exc)
       raise exc
