33cc9aeef99c7569d4fda3a2c6a4b1c566cff9b6
Alex Yakubenko <alexandr.yakubenko@gmail.com>
2023-07-21 20:21:44 -0300

    add ability to pass custom proxy model in resolve params

--- lib/nxt_gql_client/proxy_resolver.rb
+++ lib/nxt_gql_client/proxy_resolver.rb
@@ -2,9 +2,10 @@ require "nxt_gql_client/invalid_response"
 
 module NxtGqlClient
   module ProxyResolver
-    def resolve(**params)
+    def resolve(proxy_model: nil, **params)
       query_name = self.class.name.demodulize.underscore
-      Model.field_type(self.class).proxy_model.send(query_name, resolver: self, **params)
+      proxy_model ||= Model.field_type(self.class).proxy_model
+      proxy_model.send(query_name, resolver: self, **params)
     rescue InvalidResponse => exc
       handle_invalid_response_error(exc)
     end
