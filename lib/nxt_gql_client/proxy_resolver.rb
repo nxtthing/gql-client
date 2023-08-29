5db97222a3bd871460d39f1a745d8cffb3e0e12a
alexyakubenko <alexandr.yakubenko@gmail.com>
2023-08-29 16:34:55 -0300

    add ability to define proxy-model by owerwrite of resolver method

--- lib/nxt_gql_client/proxy_resolver.rb
+++ lib/nxt_gql_client/proxy_resolver.rb
@@ -3,12 +3,16 @@ require "nxt_gql_client/invalid_response"
 module NxtGqlClient
   module ProxyResolver
     def resolve(**params)
-      resolve_proxy(proxy_model: Model.field_type(self.class).proxy_model, **params)
+      resolve_proxy(**params)
     end
 
     protected
 
-    def resolve_proxy(proxy_model:, **params)
+    def proxy_model
+      Model.field_type(self.class).proxy_model
+    end
+
+    def resolve_proxy(**params)
       query_name = self.class.name.demodulize.underscore
       proxy_model.send(query_name, resolver: self, **params, context:)
     rescue InvalidResponse => exc
