709dceeaea076db0b9a716e1d6a6e65da8307540
alexyakubenko <alexandr.yakubenko@gmail.com>
2025-12-24 15:27:06 -0500

    refactor: remove hardcoded _view keys

--- lib/nxt_gql_client/model.rb
+++ lib/nxt_gql_client/model.rb
@@ -96,7 +96,7 @@ module NxtGqlClient
                        nil
                      end
           [
-            field_name == "_view" ? field_name : field_name.camelize(:lower),
+            field_name.start_with?("_") ? field_name : field_name.camelize(:lower),
             arguments,
             children
           ].join
